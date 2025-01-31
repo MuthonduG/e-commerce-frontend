3fc8a1d0fc11379adc8332f46e0956f538a770a6
MuthonduG <muthondugithinji@gmail.com>
2025-01-31 16:37:20 -0500

    update: header component screen compatibility optimization

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import React, { ReactNode } from 'react'
-import HeaderImag from '@/public/header_one.webp'
-import { SlSocialFacebook } from "react-icons/sl";
-import { FaInstagram } from "react-icons/fa";
+import HeaderImage from '@/public/header_one.webp'
+import { ImFacebook } from "react-icons/im";
+import { GrInstagram } from "react-icons/gr";
 import { FaLinkedinIn } from "react-icons/fa";
-import { MdOutlineEmail } from "react-icons/md";
+import { MdMarkEmailUnread } from "react-icons/md";
 import Link from 'next/link';
 
 type socials = {
@@ -13,10 +13,10 @@ type socials = {
 }
 
 const headerSocials: socials[] = [
-  { url: '', icon: <SlSocialFacebook /> },
-  { url: '', icon: <FaInstagram /> },
+  { url: '', icon: <ImFacebook /> },
+  { url: '', icon: <GrInstagram /> },
   { url: '', icon: <FaLinkedinIn /> },
-  { url: '', icon: <MdOutlineEmail /> },
+  { url: '', icon: <MdMarkEmailUnread /> },
 ]
 
 const Header = () => {
@@ -26,13 +26,12 @@ const Header = () => {
       {/* Background Image */}
       <div className="absolute inset-0 w-full h-full">
         <Image 
-          src={HeaderImag} 
+          src={HeaderImage} 
           alt="Header Image" 
-          layout="fill" 
-          objectFit="cover" 
+          fill 
           quality={100} 
           priority={true} 
-          className="opacity-80"
+          className="opacity-80 object-cover"
         />
       </div>
 
@@ -40,11 +39,11 @@ const Header = () => {
       <div className="relative w-full h-full flex justify-between items-center text-slate-300 md:text-4xl z-10 px-10">
         
         {/* Left Section */}
-        <div className="w-1/2 flex flex-col justify-center items-start p-6 gap-4">
-          <div className="font-semibold text-slate-50 text-left text-3xl xl:text-5xl">
+        <div className="w-[90%] md:w-1/2 xl:w-1/2 flex flex-col justify-center items-start p-6 gap-4 mt-10 md:mt-0 xl:mt-0">
+          <div className="font-semibold text-slate-50 text-left text-xl md:text-3xl xl:text-5xl">
             <span>Get A Car That Fits You</span>
           </div>
-          <div className="text-sm xl:text-lg text-left leading-8 xl:leading-10">
+          <div className="text-xs md:text-sm xl:text-lg text-left leading-8 md:leading-8 xl:leading-10">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, corrupti neque! Dolore quo amet eum voluptates iste ex natus fuga minima quisquam totam sequi commodi corporis animi qui, consectetur voluptas.
             </p>
@@ -55,10 +54,10 @@ const Header = () => {
         </div>
 
         {/* Right Section */}
-        <div className="absolute right-0 bg-rose-500 p-4 text-slate-100 text-center rounded-l-xl shadow-lg flex flex-col justify-center items-center w-fit gap-4 text-xl">
+        <div className="absolute right-0 bg-rose-500 p-4 text-slate-200 text-center rounded-l-xl shadow-lg flex flex-col justify-center items-center w-fit gap-4 text-xl">
           {
             headerSocials.map((social, index) => (
-              <Link href={social.url} key={index}>{social.icon}</Link>
+              <Link href={social.url} key={index} className='hover:text-slate-600 transition-transform delay-200 ease-in-out'>{social.icon}</Link>
             ))
           }
         </div>
